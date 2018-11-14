08653ad6cafd0485a16a456c584d532a91391a9d
KIM JONG FEEL <DeveloperFEEL@gmail.com>
2018-11-14 17:08:01 +0900

    Replace request url to https example, add img tag

--- CodeReview/no_jquery/hello.js
+++ CodeReview/no_jquery/hello.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("http://rest-service.guides.spring.io/greeting")
+    fetch("https://jsonplaceholder.typicode.com/photos/" + Math.floor((Math.random() * 5000) + 1))
     .then(response => response.json())
     .then(resolve => {
-        document.getElementsByClassName("greeting-id")[0].textContent += resolve.id;
-        document.getElementsByClassName("greeting-content")[0].textContent += resolve.content;
+        document.getElementById("photo-id").textContent += resolve.id;
+        document.getElementsByClassName("photo-title")[0].textContent += resolve.title;
+        document.getElementsByTagName("img")[0].setAttribute("src", resolve.url);
     });
 });

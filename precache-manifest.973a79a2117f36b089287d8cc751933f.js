ef80dc9c2af9b1d2c37191f4ca954ea594a27161
Kim Jong Feel <DeveloperFEEL@gmail.com>
2018-11-20 09:46:01 +0900

    Updates

---
+++ precache-manifest.973a79a2117f36b089287d8cc751933f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Hakosa2018/hakosa-demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4b538cfc2a7f8b4ffaf8",
+    "url": "/Hakosa2018/hakosa-demo/static/js/runtime~main.4b538cfc.js"
+  },
+  {
+    "revision": "51c12a5f3851fac99fb5",
+    "url": "/Hakosa2018/hakosa-demo/static/js/main.51c12a5f.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/Hakosa2018/hakosa-demo/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "51c12a5f3851fac99fb5",
+    "url": "/Hakosa2018/hakosa-demo/static/css/main.2a540601.chunk.css"
+  },
+  {
+    "revision": "7286b96be7ef90585b2d6e17c8fc54e7",
+    "url": "/Hakosa2018/hakosa-demo/index.html"
+  }
+];
